4514d4a0b591196325dd00e9a4f278b27157e5b9
chenxz <chenxiaozhen@ieforever.com>
2017-03-30 15:23:10 +0800

    mod

--- recyclerviewlibrary/src/androidTest/java/com/cxz/recyclerview/ApplicationTest.java
+++ recyclerviewlibrary/src/androidTest/java/com/cxz/recyclerview/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.cxz.recyclerlibrary;
+package com.cxz.recyclerview;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
---
+++ recyclerviewlibrary/src/main/java/com/cxz/recyclerview/SwipeRefreshLayoutOnRefresh.java
@@ -0,0 +1,19 @@
+package com.cxz.recyclerview;
+
+import android.support.v4.widget.SwipeRefreshLayout;
+
+public class SwipeRefreshLayoutOnRefresh implements SwipeRefreshLayout.OnRefreshListener {
+    PullLoadMoreRecyclerView mPullLoadMoreRecyclerView;
+
+    public SwipeRefreshLayoutOnRefresh(PullLoadMoreRecyclerView pullLoadMoreRecyclerView) {
+        this.mPullLoadMoreRecyclerView = pullLoadMoreRecyclerView;
+    }
+
+    @Override
+    public void onRefresh() {
+        if (!mPullLoadMoreRecyclerView.isRefresh()) {
+            mPullLoadMoreRecyclerView.setIsRefresh(true);
+            mPullLoadMoreRecyclerView.refresh();
+        }
+    }
+}
